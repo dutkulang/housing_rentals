435e64e255892379e6ada11dd4914bf441078b47
Dut Kulang <dutandrew78@gmail.com>
2025-10-11 21:37:27 +0200

    create middle functions to check login and signup operations.
    loginChecker checks if all the necessary login fields and request.body
    exist then pass the execution over to next()
    
    signupChecker checks if necessary required signup fields are present
    and then pass execution over to next()

---
+++ backend/src/middleware/signupChecker.js
@@ -0,0 +1,25 @@
+import validator from "validator"
+
+function signupChecker(request, response, next){
+    const requiredFields = ["fullName", "email", "password"]
+    try {
+        if(!request.body){
+            throw new Error("Invalid request body")
+        }
+        const {email} = request.body
+        if(!validator.isEmail(email)){
+            throw new Error("Invalid credentials")
+        }
+        const requestKeys = Object.keys(request.body)
+        if(!requestKeys.every(key=> requiredFields.includes(key))){
+            throw new Error("Invalid credentials")
+        }
+        next()
+    } catch (error) {
+        response.status(400).json({
+            error: error.message
+        })
+    }
+}
+
+export default signupChecker
