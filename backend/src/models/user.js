08d045e766d6f54f5a7909d6283e92d2d45b3bc8
Dut Kulang <dutandrew78@gmail.com>
2025-10-11 23:18:36 +0200

    ensure that user email is unique

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -14,6 +14,7 @@ const userSchema = new Schema({
         required: true,
         lowercase: true,
         trim: true,
+        unique: true,
         validate(value){
             if(!validator.isEmail(value)){
                 throw new Error("Invalid email");
