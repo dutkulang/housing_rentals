9c41860fce1044ba0d8f9e8205a83df48f398d93
Dut Kulang <dutandrew78@gmail.com>
2026-01-08 18:30:42 +0200

    Made demo properties as link to property page

--- frontend/src/components/PropertyList.jsx
+++ frontend/src/components/PropertyList.jsx
@@ -1,15 +1,24 @@
+import { Link } from "react-router";
 function PropertyList(){
     return (
         <div>
+            <Link to={"/property/123"}>
             <div>
                 <h1>property 1</h1>
             </div>
-            <div>
-                <h1>property 2</h1>
-            </div>
-            <div>
-                <h1>property 3</h1>
-            </div>
+            </Link>
+            <Link to={"/property/123"}>
+                <div>
+                    <h1>property 2</h1>
+                </div>
+            </Link>
+            
+            <Link to={"/property/123"}>
+                <div>
+                    <h1>property 3</h1>
+                </div>
+            </Link>
+            
         </div>
     )
 }
