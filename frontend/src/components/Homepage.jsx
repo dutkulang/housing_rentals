88f904e455565b8f05f0de2854f1b14f05e89dff
Dut Kulang <dutandrew78@gmail.com>
2026-01-08 16:38:21 +0200

    config basic routing in project

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,10 +1,10 @@
 import Header from "./Header";
-
+import { Outlet } from "react-router";
 function Homepage(){
     return (
         <>
             <Header/>
-
+            <Outlet/>
         </>
     )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider } from "react-router";
+import Error from './components/Error.jsx';
+import Homepage from './components/Homepage';
+import Property from './components/property.jsx';
+import PropertyList from './components/PropertyList.jsx';
 import './index.css'
 import App from './App.jsx'
 
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <Homepage /> ,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <PropertyList />
+      },
+      {
+        path:"/property/123",
+        element: <Property/>
+      }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={appRouter} />
 )
