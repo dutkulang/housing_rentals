fc9fb2e4bf20d06240a53692dbc4497c93a4ad7a
Dut Kulang <dutandrew78@gmail.com>
2026-01-08 18:24:31 +0200

    Add detail error infomation using react router's useRouteError hook

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,7 +1,13 @@
+import { useRouteError, Link } from "react-router";
+
 function Error(){
+    const error = useRouteError()
+    console.log(error)
     return (
         <div>
-            <h1>Ooops Error</h1>
+            <h1>Ooops!!! {error.status}</h1>
+            <h2>{error.statusText}</h2>
+            <p><Link to="/">Back Home</Link></p>
         </div>
     )
 }
