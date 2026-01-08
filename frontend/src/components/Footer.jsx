b392e2c177f33ce656cb2a66644ade0c4e40fad6
Dut Kulang <dutandrew78@gmail.com>
2026-01-08 18:25:15 +0200

    Add footer component with basic footer information

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    
+    return (
+        <div>
+            <p>{new Date().getFullYear()} &copy; Juwa estate | made with ❤ Dut Kulang</p>
+        </div>
+    )
+}
+
+export default Footer;
